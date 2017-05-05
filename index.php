de0d6e09b23eb4af9f3a376fadca0a967f8eb14c
cybergrouch <cyber_grouch_lange@yahoo.ie>
2017-05-05 10:28:38 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Assignment 1: Web Technologies and Development (SIT774)</title>
+        <meta name="description" content="Assignment 2: Web Technologies and Development (SIT774)">
+        
+        <link href="css/bootstrap.css" rel="stylesheet">
+    </head>
+    <body>
+        <!-- Add your site or application content here -->
+        <p>Hello world! This is HTML5 Boilerplate.</p>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="js/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+
+    </body>
+</html>
